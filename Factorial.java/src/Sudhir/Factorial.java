0dc482ac67bd96f34a913e96787cf062eecdeb58
244306 <244306@SUDHIR>
2022-05-13 21:04:09 +0530

    The Code update

---
+++ Factorial.java/src/Sudhir/Factorial.java
@@ -0,0 +1,19 @@
+package Sudhir;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		  int no = 5;
+		  int fact = 1;
+		           for(int i =1;i<no;i++)
+		            {
+		               fact  = fact * i;
+		                    }
+		                  System.out.println("Factorial: " + fact);
+		                }
+
+	}
